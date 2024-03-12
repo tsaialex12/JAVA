d59a2294a0e2ec970a047d3836868bd8211a0312
tsaialex12 <112999798+tsaialex12@users.noreply.github.com>
2024-03-12 10:37:22 +0800

    Add files via upload

--- JAVA02/JPA204.java
+++ JAVA02/JPA204.java
@@ -2,16 +2,15 @@ import java.util.*;
 class JPA204 {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        test();
-        test();
-    }
-  
-    public static void test() {
+    for (int i = 0;i <=1;i++){
         System.out.println("Input:");
         int n = input.nextInt();
-        if(n % 5 == 0 && n % 9 == 0)
-        	System.out.println("Yes");
-        else
-        	System.out.println("No");
+        if(n % 5 == 0 && n % 9 == 0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+            }
+        }
     }
 }
--- JAVA02/JPA205.java
+++ JAVA02/JPA205.java
@@ -2,16 +2,9 @@ import java.util.*;
 public class JPA205 {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        test();
-        test();
-        test();
-        test();
-    }
-    
-    static void test() {
         System.out.println("Enter an integer:");
-        int n = input.nextInt();
-        
+        for(int i = 0;i<=3;i++){
+            int n = input.nextInt();
         if(n % 6 == 0)
         	System.out.printf("%d是2、3、6的倍數\n", n);
         else if(n % 3 == 0)
@@ -20,5 +13,6 @@ public class JPA205 {
         	System.out.printf("%d是2的倍數\n", n);
         else
         	System.out.printf("%d不是2、3、6的倍數\n", n);
+        }        
     }
 }
